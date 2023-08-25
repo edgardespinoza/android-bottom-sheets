e82acc09cccbf171804e0498e0fdb2b358e492df
eespinor <edgard.espinoza.rivas@gmail.com>
2023-08-25 08:18:16 -0500

    add scaffold

--- app/src/main/java/com/eespinor/bottomsheets/MainActivity.kt
+++ app/src/main/java/com/eespinor/bottomsheets/MainActivity.kt
@@ -6,19 +6,29 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.eespinor.bottomsheets.ui.theme.BottomSheetsTheme
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
@@ -32,26 +42,53 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     val sheetState = rememberModalBottomSheetState()
+                    var isSheetOpen by rememberSaveable {
+                        mutableStateOf(false)
+                    }
+                    val scaffoldState = rememberBottomSheetScaffoldState()
 
-                    Box(
-                        modifier = Modifier.fillMaxSize(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Button(onClick = {
+                    val scope = rememberCoroutineScope()
 
-                        }) {
-                            Text(text = "")
+                    BottomSheetScaffold(
+                        scaffoldState = scaffoldState,
+                        sheetContent = {
+                            Image(
+                                painter = painterResource(id = R.drawable.kermit),
+                                contentDescription = null,
+                                modifier = Modifier.fillMaxWidth ()
+
+                            )
+                        },
+                        sheetPeekHeight = 0.dp
+                        ) {
+
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Button(onClick = {
+                                scope.launch {
+                                    scaffoldState.bottomSheetState.expand()
+                                }
+                            }) {
+                                Text(text = "Open sheet")
+                            }
                         }
                     }
 
-                    ModalBottomSheet(
-                        sheetState = sheetState,
-                        onDismissRequest = { /*TODO*/ }) {
-                        Image(
-                            painter = painterResource(id = R.drawable.kermit),
-                            contentDescription = null
-                        )
-                    }
+//                    if (isSheetOpen) {
+//                        ModalBottomSheet(
+//                            sheetState = sheetState,
+//                            onDismissRequest = {
+//                                isSheetOpen = false
+//                            }
+//                        ) {
+//                            Image(
+//                                painter = painterResource(id = R.drawable.kermit),
+//                                contentDescription = null
+//                            )
+//                        }
+//                    }
                 }
             }
         }
